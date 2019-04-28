98a61c975f8ee4338e7383bf0c6c144adb229fed
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-27 22:32:50 -0300

    feat: Add theme colors

---
+++ utils/theme.js
@@ -0,0 +1,13 @@
+const black = '#000'
+const lightBlue = '#949cdd'
+const darkBlue = '#5366e7'
+const white = '#fff'
+
+const theme = {
+  black,
+  darkBlue,
+  lightBlue,
+  white,
+}
+
+export default theme
