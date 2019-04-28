3bce90a44e5def97a8ac98037579cea12357b741
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-28 00:12:49 -0300

    feat: Add deck reducer

---
+++ reducers/decks.js
@@ -0,0 +1,31 @@
+import { handleActions } from 'redux-actions'
+import { fetchDecks, saveCard } from '../actions/deck'
+
+const initialState = {
+  decks: [],
+}
+
+const reducer = handleActions(
+  {
+    [fetchDecks]: (state, action) => ({
+      ...state,
+      ...action.payload,
+    }),
+    [saveCard]: (state, action) => ({
+      ...state,
+      decks: state.decks.map(deck => {
+        if (deck.title === action.payload.deck.title) {
+          return {
+            ...deck,
+            cards: [...deck.cards, action.payload.card],
+          }
+        }
+
+        return deck
+      }),
+    }),
+  },
+  initialState,
+)
+
+export default reducer
