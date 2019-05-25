4d9ffbffe78e889505c9ad96c34ec0102df88bf0
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 17:25:22 -0300

    fix: Sanitize decks response

--- actions/deck.js
+++ actions/deck.js
@@ -5,15 +5,20 @@ export const fetchDecks = createAction('FETCH_DECKS')
 export const saveDeck = createAction('SAVE_DECK')
 export const saveCard = createAction('SAVE_CARD')
 
+const sanitizeDecks = decks => {
+  if (decks === null) return []
+  return JSON.parse(decks)
+}
+
 export const loadDecks = () => async dispatch => {
   const response = await getDecks()
-  const decks = JSON.parse(response)
+  const decks = sanitizeDecks(response)
   return dispatch(fetchDecks({ decks }))
 }
 
 export const addDeck = deck => async dispatch => {
   const response = await getDecks()
-  const decks = JSON.parse(response)
+  const decks = sanitizeDecks(response)
   await updateDecks([...decks, deck])
   return dispatch(saveDeck({ deck }))
 }
