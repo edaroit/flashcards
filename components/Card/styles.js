3af018aaddc7bbd0bdcfafd1ce3ee8fe272b1e3f
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 16:53:27 -0300

    feat: Add Card component

---
+++ components/Card/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Card = styled.View`
+  border: 1px solid ${props => props.theme.shadow};
+  margin: 10px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px ${props => props.theme.shadow};
+  background-color: ${props => props.theme.white};
+  padding: 10px;
+`
