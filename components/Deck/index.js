bf18f2b9e7ab43b03bafc2418e8db4894334c34f
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 18:04:11 -0300

    fix: Remove nonsense default

--- components/Deck/index.js
+++ components/Deck/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Count, Title } from './styles'
 import Card from '../Card'
 
-export default ({ title = 'ueba', cards = [] }) => (
+export default ({ title = '', cards = [] }) => (
   <Card>
     <Title>{title}</Title>
     <Count>{cards.length}</Count>
