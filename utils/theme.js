83ca072dca090ff106affa9d5dd9951cda759095
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 16:52:44 -0300

    feat: Add new colors

--- utils/theme.js
+++ utils/theme.js
@@ -1,12 +1,16 @@
 export const black = '#000'
-export const lightBlue = '#949cdd'
 export const darkBlue = '#5366e7'
+export const gray = '#f4f4f4'
+export const lightBlue = '#949cdd'
+export const shadow = '#ccc'
 export const white = '#fff'
 
 const theme = {
   black,
   darkBlue,
+  gray,
   lightBlue,
+  shadow,
   white,
 }
 
