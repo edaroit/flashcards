a28843c446beb7f154e551a82237113db34b7319
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 17:26:15 -0300

    feat: Connect Decks reducer to Home

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Container, Header, Title } from './styles'
+import Deck from '../Deck'
+import { loadDecks } from '../../actions/deck'
 
-export default () => (
-  <Container>
-    <Header>
-      <Title>Flashcards</Title>
-    </Header>
-  </Container>
-)
+class Home extends Component {
+  componentDidMount() {
+    // eslint-disable-next-line react/destructuring-assignment
+    this.props.loadDecks()
+  }
+
+  render() {
+    const { decks } = this.props
+    return (
+      <Container>
+        <Header>
+          <Title>Flashcards</Title>
+        </Header>
+        {decks.map(deck => (
+          <Deck {...deck} />
+        ))}
+      </Container>
+    )
+  }
+}
+
+export default connect(
+  state => ({ decks: state.decks.decks }),
+  dispatch => bindActionCreators({ loadDecks }, dispatch),
+)(Home)
