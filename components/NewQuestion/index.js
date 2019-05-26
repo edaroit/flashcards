f63f898bb2c07111e9e66ff3de0d8ffdcbb49f9b
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 21:01:55 -0300

    feat: Add NewQuesiton form

--- components/NewQuestion/index.js
+++ components/NewQuestion/index.js
@@ -1,4 +1,51 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { addCard } from '../../actions/deck'
+import { Button } from './styles'
 
-export default () => <SafeAreaView />
+class NewQuestion extends Component {
+  constructor() {
+    super()
+    this.state = {
+      question: '',
+      answer: '',
+    }
+  }
+
+  handleSubmit() {
+    const { navigation } = this.props
+    const { question, answer } = this.state
+    const deckId = navigation.getParam('deckId')
+    // eslint-disable-next-line react/destructuring-assignment
+    this.props.addCard(deckId, { question, answer })
+    navigation.goBack()
+    this.setState({ question: '', answer: '' })
+  }
+
+  render() {
+    const { question, answer } = this.state
+
+    return (
+      <SafeAreaView>
+        <TextInput
+          value={question}
+          placeholder="What's the worst React derivate?"
+          onChangeText={value => this.setState({ question: value })}
+        />
+        <TextInput
+          value={answer}
+          placeholder="React Native"
+          onChangeText={value => this.setState({ answer: value })}
+        />
+        <Button onPress={() => this.handleSubmit()} title="Send" />
+      </SafeAreaView>
+    )
+  }
+}
+
+export default connect(
+  null,
+  dispatch => bindActionCreators({ addCard }, dispatch),
+)(NewQuestion)
