03ea2fee20f6a61d1e751e4927638ae6b9d3188c
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 21:01:22 -0300

    fix: Corect API logics

--- utils/api.js
+++ utils/api.js
@@ -2,15 +2,26 @@ import { AsyncStorage } from 'react-native'
 
 const DECKS_STORAGE_KEY = 'Flashcards:decks'
 
-export const getDecks = () => AsyncStorage.getItem(DECKS_STORAGE_KEY)
+export const getDecks = () => {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const decks = JSON.parse(results)
+    if (decks === null) return []
+    return decks
+  })
+}
 
-export const postCard = (deck, card) =>
-  AsyncStorage.mergeItem(
-    DECKS_STORAGE_KEY,
-    JSON.stringify({
-      [deck]: card,
-    }),
-  )
+export const postCard = (deckId, card) => {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const decks = JSON.parse(results)
+    const deck = decks.find(d => d.id === deckId)
+    const updatedDeck = { ...deck, cards: [...deck.cards, card] }
+
+    AsyncStorage.setItem(
+      DECKS_STORAGE_KEY,
+      JSON.stringify([...decks.filter(d => d.id !== deckId), updatedDeck]),
+    )
+  })
+}
 
 export const updateDecks = decks =>
   AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks))
