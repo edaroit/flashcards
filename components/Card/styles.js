65142dcc9b77e619f7f2f8a3f37e7f8450a9de2c
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-26 14:03:04 -0300

    refactor: Update Card styles

--- components/Card/styles.js
+++ components/Card/styles.js
@@ -6,5 +6,5 @@ export const Card = styled.View`
   border-radius: 10px;
   box-shadow: 0 0 10px ${props => props.theme.shadow};
   background-color: ${props => props.theme.white};
-  padding: 10px;
+  padding: 20px;
 `
