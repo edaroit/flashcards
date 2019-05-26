7d858f3ef2228d0cef299fa639d53431f7e6fa26
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-05-25 21:54:18 -0300

    feat: Add NewDeck form

--- components/NewDeck/index.js
+++ components/NewDeck/index.js
@@ -1,4 +1,44 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import uuid from 'uuid/v4'
+import { addDeck } from '../../actions/deck'
+import { Button } from './styles'
 
-export default () => <SafeAreaView />
+class NewDeck extends Component {
+  constructor() {
+    super()
+    this.state = { title: '' }
+  }
+
+  async handleSubmit() {
+    const { navigation } = this.props
+    const { title } = this.state
+    const id = uuid()
+    // eslint-disable-next-line react/destructuring-assignment
+    await this.props.addDeck({ id, title })
+    navigation.navigate('DeckDetails', { deckId: id })
+    this.setState({ title: '' })
+  }
+
+  render() {
+    const { title } = this.state
+
+    return (
+      <SafeAreaView>
+        <TextInput
+          value={title}
+          placeholder="React Native Studies"
+          onChangeText={value => this.setState({ title: value })}
+        />
+        <Button onPress={() => this.handleSubmit()} title="Send" />
+      </SafeAreaView>
+    )
+  }
+}
+
+export default connect(
+  null,
+  dispatch => bindActionCreators({ addDeck }, dispatch),
+)(NewDeck)
---
+++ components/NewDeck/styles.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components/native'
+import { Button as BaseButton } from 'react-native-elements'
+
+export const Button = styled(BaseButton)``
